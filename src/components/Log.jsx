9a722f10a2c619769e899b2e66ac1650f97ebe0e
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-11-09 16:20:21 +0100

    add log info

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,5 +1,14 @@
-const Log = () => {
-  return <ol id='log'></ol>;
+/* eslint-disable react/prop-types */
+const Log = ({ turns }) => {
+  return (
+    <ol id='log'>
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected {turn.square.row}, {turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
 };
 
 export default Log;
