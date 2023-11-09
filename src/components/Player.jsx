75d055f5124f94215db647c08e28b10e28dbe6fb
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-11-09 14:55:23 +0100

    add user input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,20 +1,34 @@
 import { useState } from 'react';
 
 /* eslint-disable react/prop-types */
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
+
   const handleEditClick = () => {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
   };
+
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
   return (
     <>
       <li>
         <span className='player'>
-          {isEditing && <input type='text' value={name} required />}
-          {!isEditing && <span className='player-name'>{name}</span>}
+          {isEditing && (
+            <input
+              type='text'
+              value={playerName}
+              onChange={handleChange}
+              required
+            />
+          )}
+          {!isEditing && <span className='player-name'>{playerName}</span>}
           <span className='player-symbol'>{symbol}</span>
         </span>
-        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
       </li>
     </>
   );
