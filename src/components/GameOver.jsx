61aa27243c7c401509334be49f9a4a8654e6166a
Krzysztof Zieliński <kzielinski50@gmail.com>
2023-11-09 17:16:19 +0100

    add rematch option

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
-const GameOver = ({ winner }) => {
+const GameOver = ({ winner, onRestart }) => {
   return (
     <div id='game-over'>
       <h2>Game Over!</h2>
       {winner && <p>{winner} won!</p>}
       {!winner && <p>It&apos;s a draw!</p>}
       <p>
-        <button>Rematch!</button>
+        <button onClick={onRestart}>Rematch!</button>
       </p>
     </div>
   );
